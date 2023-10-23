b6fb67928c02b3f02faeeddb9bd043367c31da1d
= <=>
2023-10-23 16:58:18 +0200

    day 1

---
+++ fct_training.R
@@ -0,0 +1,99 @@
+#food composition tables
+# https://dzvoti.github.io/r4hces/fct_standardisation.html
+
+rm(list = ls())
+
+# loading libraries
+
+library(readxl)
+library(stringr)
+library(dplyr)
+library(here)
+
+
+f <- "https://www.fao.org/fileadmin/user_upload/faoweb/2020/WAFCT_2019.xlsx"
+
+
+download.file(f, 
+              destfile = here::here("data", # data folder
+                                    'WA19', #FCT folder
+                                    #FCT file
+                                    "WAFCT_2019.xlsx"),  
+              method="curl", #use "curl" for OS X / Linux, "wininet" for Windows
+              mode="wb")
+
+# This identifies the file and file path, and saves it as a variable
+FCT_file_location <- here::here('data','WA19', "WAFCT_2019.xlsx") 
+
+# This is an example of the name 
+FCT_id <- 'WA19'# Change two first letter for your ISO 2 code & the two digits for the last two digits of the year of publication.
+
+
+# Checking the sheets
+
+readxl::excel_sheets(FCT_file_location)
+
+data.df <- readxl::read_excel(FCT_file_location, #The file location, as identified in section 2.1
+                              sheet = 5  # Change to the excel sheet where the FCT is stored in the excel file
+) %>%  
+  mutate(source_fct = FCT_id)  #Creates the source_fct column and fills with a id for this FCT, as filled in in section 2.2. 
+
+
+# Checking the dataframe
+head(data.df) 
+
+dim(data.df)
+
+# Structure (variable names, class, etc.)
+str(data.df)
+
+# Checking the last rows and columns
+tail(data.df) 
+names(data.df)
+
+print(data.df %>% slice(1:5) %>% knitr::kable())
+
+# Storing the variables you want to keep
+columns_to_keep <- c('Scientific name', 'Energy\r\n(kJ)')
+
+# Selecting the variables
+data.df %>% select(columns_to_keep) %>% 
+  head(5) 
+
+columns_to_remove <- c('Food name in French', 'Sum of proximate components\r\n(g)') 
+
+data.df %>% select(!columns_to_remove) %>% 
+  head(5) 
+
+
+#Creates a list of the food groups using their unique row structure in the table to identify them
+
+fgroup <- data.df %>% 
+  filter(is.na(`Food name in English`), !is.na(Code)) %>%
+  pull(Code) %>%
+  stringr::str_split_fixed( '/', n = 2) %>% 
+  as_tibble() %>%
+  pull(V1) 
+
+group.id <-  unique(str_extract(data.df$Code, "^[:digit:]{2}\\_"))[-1]
+
+
+
+# Removes any rows without a food description entry (the food group name rows, and a row that have already been used for naming)
+
+data.df <- data.df %>% #Identifies the food group number from the fdc_id, and applies the correct food_group from the fgroup list to the food_group column 
+  mutate(food_group = ifelse(grepl("01_", Code), fgroup[1],
+                             ifelse(grepl("02_", Code), fgroup[2],
+                             ifelse(grepl("03_", Code), fgroup[3],
+                             ifelse(grepl("04_", Code), fgroup[4],
+                             ifelse(grepl("05_", Code), fgroup[5],
+                             ifelse(grepl("06_", Code), fgroup[6],
+                             ifelse(grepl("07_", Code), fgroup[7],
+                             ifelse(grepl("08_", Code), fgroup[8],
+                             ifelse(grepl("09_", Code), fgroup[9], 
+                             ifelse(grepl("10_", Code), fgroup[10], 
+                             ifelse(grepl("11_", Code), fgroup[11], 
+                             ifelse(grepl("12_", Code), fgroup[12],
+                             ifelse(grepl("13_", Code), fgroup[13],
+                             ifelse(grepl("14_", Code), fgroup[14], 
+                             'NA')))))))))))))))
--- training_day1.R
+++ training_day1.R
@@ -1,5 +1,10 @@
 # R for HCES training 
 # MAPS team
+# https://dzvoti.github.io/r4hces/fct_standardisation.html
+library(dplyr)
+library(readr)
+library(haven)
+library(here)
 
 food_name <- "Maize"
 food_subname <- "Meal"
@@ -90,3 +95,126 @@ food_df <-
 # Print the data frame
 print(food_df)
 tail(food_df)
+
+food_df <- data.frame(food_names, consumption, is_staple, stringsAsFactors = TRUE)
+
+
+# Create a tibble
+food_tb <- tibble::tibble(
+  food_names = c(
+    "Rice",
+    "Maize",
+    "Beans",
+    "Cassava",
+    "Potatoes",
+    "Maize",
+    "Wheat"
+  ),
+  consumption = c(0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.01),
+  is_staple = c(TRUE, TRUE, TRUE, TRUE, FALSE, TRUE, FALSE)
+)
+# check which class they are in 
+class(food_t)
+class(food_tb)
+
+
+#### vectors as factors
+food_names_factor <- factor(food_names)
+# Create a factor from a vector of character values
+food_names_factor_2 <-
+  factor(
+    food_names,
+    levels = c(
+      "Beans",
+      "Rice",
+      "Maize",
+
+      "Cassava",
+      "Potatoes",
+      "Sweet potatoes",
+      "Wheat"
+    )
+  )
+
+# Print the factor
+print(food_names_factor_2)
+
+
+food_tb <- food_tb %>% 
+  dplyr::mutate(food_names = factor(food_names))
+
+
+
+
+
+### create functions
+
+# Define a function
+multiply <- function(arg1, arg2, ...) {
+  # Function body
+  return_value <- arg1 * arg2
+  # Return value
+  return(return_value)
+}
+
+
+
+
+
+#### Input and output ----------------------------------------------------------
+
+ihs5_roster <- haven::read_dta(here::here("mwi-ihs5-sample-data", "hh_mod_a_filt_vMAPS.dta"))
+
+# Import unit conversion factors data 
+ihs5_unit_conversion_factors <- read_csv(here::here("mwi-ihs5-sample-data", "IHS5_UNIT_CONVERSION_FACTORS_vMAPS.csv"))
+head(ihs5_unit_conversion_factors)
+
+# Consumption data
+
+ihs5_consumption <- 
+head(ihs5_consumption)
+dim(ihs5_consumption)
+
+# Subset the data
+ihs5_consumption <-
+  read_dta(here::here("mwi-ihs5-sample-data/HH_MOD_G1_vMAPS.dta")) %>% 
+  dplyr::select(
+    case_id,
+    HHID,
+    hh_g01,
+    hh_g01_oth,
+    hh_g02,
+    hh_g03a,
+    hh_g03b,
+    hh_g03b_label,
+    hh_g03b_oth,
+    hh_g03c,
+    hh_g03c_1
+  ) %>% 
+  dplyr::rename(
+       consumedYN = hh_g01,
+       food_item = hh_g02,
+       food_item_other = hh_g01_oth,
+       consumption_quantity = hh_g03a,
+       consumption_unit = hh_g03b,
+       consumption_unit_label = hh_g03b_label,
+       consumption_unit_other = hh_g03b_oth, 
+       consumption_subunit_1 = hh_g03c, 
+       consumption_subunit_2 = hh_g03c_1
+       ) %>% 
+  filter(consumedYN == 1)
+
+ihs5_consumption <- ihs5_consumption %>% 
+  dplyr::mutate(hh_members = sample(1:10, nrow(ihs5_consumption), replace = TRUE))
+
+
+ihs5_consumption <- ihs5_consumption |>
+  mutate(consumption_per_person = consumption_quantity / hh_members)
+### assuming a 7-day recall
+
+ihs5_consumption <- ihs5_consumption %>% 
+  mutate(consumption_per_person_per_day = consumption_per_person/7)
+
+
+
+
